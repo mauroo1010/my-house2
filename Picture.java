66a6e03159b87111d470bb59f7a9a87cdd7b0c92
Mauro <maurogutierreztormo@gmail.com>
2023-10-17 12:16:52 +0200

    Segundo commit

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle moon;
 
     /**
      * Constructor for objects of class Picture
@@ -54,6 +55,13 @@ public class Picture
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        moon = new Circle();
+        moon.makeVisible();
+        moon.changeColor("magenta");
+        moon.moveHorizontal(-100);
+        moon.moveVertical(-40);
+        moon.changeSize(40);
     }
 
     /**
